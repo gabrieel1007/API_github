b1ff7bc8e4b053d67bd051b710232f17cc262a8c
Gabriel Alves <ga654066@gmail.com>
2023-10-20 14:30:50 -0300

    API github

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const repositories = document.querySelector('.content-main');
+
+function getApiGitHub(){
+    fetch('https://api.github.com/users/barbosaxbarbosa/repos')
+    .then(async res =>{
+        if(!res.ok){
+            throw new Error(res.status);
+        }else{
+            let data = await res.json();
+            data.map(item => {
+                let project = document.createElement('div');
+
+                project.innerHTML = `
+                <div class="project">
+                    <div>
+                        <h4 class="title">${ item.name}</h4>
+                        <span class="date-create">${ Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+                    </div>
+                    <div>
+                        <a href="${ item.html_url}" target="_blank">${ item.html_url}</a>
+                        <span class="language"><span class="circle"></span>${ item.language}</span>
+                    </div>
+                </div>
+                `;
+
+                repositories.appendChild(project);
+            })
+        }
+    })
+}
+getApiGitHub();
